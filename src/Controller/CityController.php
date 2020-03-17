dd4a8522a7f22804d2bae2875cc6c0b55cc92381
Iman <ipernin@humanbooster.com>
2020-03-17 17:00:32 +0100

    route

--- src/Controller/CityController.php
+++ src/Controller/CityController.php
@@ -2,18 +2,55 @@
 
 namespace App\Controller;
 
+use App\Entity\City;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CityController extends AbstractController
-{
-    /**
-     * @Route("/city", name="city")
+{ 
+    /** 
+    * * Affiche la liste de toutes les villes 
+     * @Route("/city", name="city_index")
      */
     public function index()
     {
         return $this->render('city/index.html.twig', [
             'controller_name' => 'CityController',
         ]);
+    }
+
+    /**
+     * Affiche le détail d'une ville 
+     * @Route("/city/{id}", name="city_show", methods={"GET"})
+     * @param City $city
+     */
+    public function show(City $city)
+    {
+    }
+
+    /**
+     * Traite la requête d'un formulaire de création d'une ville
+     * @Route("/city", name="city_create", methods={"POST"})
+     */
+    public function create()
+    {
+    }
+    /**
+     * Affiche le formulaire d'édition d'une ville  (GET)
+     * Traite le formulaire d'édition d'une ville  (POST)
+     * @Route("/city/{id}/edit", name="city_edit", methods={"GET", "POST"})
+     * @param City $city
+     */
+    public function edit(City $city)
+    {
+    }
+
+    /**
+     * Supprime une ville
+     * @Route("/city/{id}", name="city_delete", methods={"DELETE"})
+     * @param City $city
+     */
+    public function delete(City $city)
+    {
     }
 }
